cf0df596b4f2f9acddaf2a048cf46134e35eb9a0
Hariom Chouhan <hariomchouhan430@gmail.com>
2023-08-26 14:45:28 +0530

    Add MilletAahaar project and update resume

--- src/ContextAPI/ThemeContext.js
+++ src/ContextAPI/ThemeContext.js
@@ -3,7 +3,7 @@ import { createContext, useState } from "react";
 export const themeContext = createContext();
 
 export default function ThemeContextProvider({ children }) {
-  const [color, setColor] = useState("#4169e1");
+  const [color, setColor] = useState("#004aad");
   const [boxShadow, setBoxShadow] = useState("0 20px 25px -5px #6957af");
 
   const value = {
